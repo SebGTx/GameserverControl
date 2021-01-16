f6dcefe953fef89115636af8847f90386f6a60bf
SebGTx <8062146+SebGTx@users.noreply.github.com>
2021-01-16 17:02:35 +0100

    Delete Rights.bat

--- GameserverControl/Resources/Rights.bat
+++
@@ -1,3 +0,0 @@
-REM netsh http add urlacl url="http://+:8080/" user="Tout le monde"
-netsh http add urlacl url="http://+:8080/" user=%USERNAME%
-netsh advfirewall firewall add rule name= "GameserverControl" dir=in action=allow protocol=TCP localport=8080
